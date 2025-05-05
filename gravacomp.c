dce48d1f3993e107d26f89181e48c635f035022f
jomigmonte <jomigmonte@gmail.com>
2025-05-05 00:23:31 -0300

    quase todos os comentários

--- gravacomp.c
+++ gravacomp.c
@@ -4,39 +4,64 @@
 #include "gravacomp.h"
 
 int calculaIndice(int indiceAtual, int tamanhoCampo);
-void codificaString(char* str, char* descritor, FILE* arquivo);
-void codificaInt(int* num, char* descritor, FILE* arquivo);
-void codificaU(unsigned int* num, char* descritor, FILE* arquivo);
+int codificaString(char* str, char* descritor, FILE* arquivo);
+int codificaInt(int* num, char* descritor, FILE* arquivo);
+int codificaU(unsigned int* num, char* descritor, FILE* arquivo);
 
 int gravacomp(int nstructs, void* valores, char* descritor, FILE* arquivo)
 {
 	int i, j, indiceEstrutura = 0;
 	char hasInt;
 
-	fprintf(arquivo, "%c", (unsigned char)nstructs);
+	int err;
+	//grava no arquivo o numero de estruturas que serao gravadas
+	err = fprintf(arquivo, "%c", (unsigned char)nstructs);
+	//a funcao fprintf retorna um valor negativo em caso de erro entao nesse caso a funcao gravacomp retorna -1
+	if (err < 0)
+	{
+		return -1;
+	}
+	//percorre as estruturas 
 	for (i = 0; i < nstructs; i++)
 	{
 		j = 0;
 		hasInt = 0;
 
+		//percorre a string do descritor
 		while (descritor[j] != '\0')
 		{
+			//realiza passos diferentes em cada um dos casos do tipo de dado na estrutura
 			switch (descritor[j])
 			{
 			case 'i':
 				indiceEstrutura = calculaIndice(indiceEstrutura, sizeof(int));
-				codificaInt((int*)((char*)valores + indiceEstrutura), descritor + j, arquivo);
+				err = codificaInt((int*)((char*)valores + indiceEstrutura), descritor + j, arquivo);
+				//a funcao fprintf retorna um valor negativo em caso de erro entao nesse caso a funcao gravacomp retorna -1
+				if (err < 0)
+				{
+					return -1;
+				}
 				indiceEstrutura += sizeof(int);
 				hasInt = 1;
 				break;
 			case 'u':
 				indiceEstrutura = calculaIndice(indiceEstrutura, sizeof(unsigned));
-				codificaU((unsigned*)((char*)valores + indiceEstrutura), descritor + j, arquivo);
+				err = codificaU((unsigned*)((char*)valores + indiceEstrutura), descritor + j, arquivo);
+				//a funcao fprintf retorna um valor negativo em caso de erro entao nesse caso a funcao gravacomp retorna -1
+				if (err < 0)
+				{
+					return -1;
+				}
 				indiceEstrutura += sizeof(unsigned);
 				hasInt = 1;
 				break;
 			case 's':
-				codificaString((char*)valores + indiceEstrutura, descritor + j, arquivo);
+				err = codificaString((char*)valores + indiceEstrutura, descritor + j, arquivo);
+				//a funcao fprintf retorna um valor negativo em caso de erro entao nesse caso a funcao gravacomp retorna -1
+				if (err < 0)
+				{
+					return -1;
+				}
 				indiceEstrutura += (descritor[j + 1] - '0') * 10 + descritor[j + 2] - '0';
 				j += 2;
 				break;
@@ -44,9 +69,11 @@ int gravacomp(int nstructs, void* valores, char* descritor, FILE* arquivo)
 			j++;
 		}
 
+		//se tiver lidando com um inteiro, chama uma funcao de apoio para calcular o indice por conta do padding necessario
 		if (hasInt)
 			indiceEstrutura = calculaIndice(indiceEstrutura, sizeof(int));
 	}
+	return 0;
 }
 
 void mostracomp(FILE* arquivo)
@@ -54,25 +81,30 @@ void mostracomp(FILE* arquivo)
 	unsigned char nEstruturas = 0;
 	unsigned char charLido;
 	
+	//le o primeiro caractere do arquivo binario que indica o numero de estruturas e mostra ele na tela
 	fscanf(arquivo, "%c", &nEstruturas);
 	printf("Estruturas: %u\n\n", nEstruturas);
 
+	//percorre as estruturas do arquivo com base no numero total lido anteriormente
 	for (int j = 0; j < nEstruturas; j++)
 	{
 		charLido = 0;
 
+		//continua o loop ate o bit que indica se a estrutura e a ultima for 1 
 		while (!(charLido & 0x80))
 		{
 			fscanf(arquivo, "%c", &charLido);
 
+			//checa se o bit (ou os no caso de um inteiro) lido que define o tipo de dado e de string, int ou unsigned int
 			if (charLido & 0x40)
 			{
-				//Str
+				//le os bytes que indicam o tamanho da string
 				int tamanhoStr = charLido & 0x3f;
 				char caracterString;
 
 				printf("(str) ");
 
+				//com base no tamanho lido, vai lendo os caracteres da string e mostrando na tela
 				for (int i = 0; i < tamanhoStr; i++)
 				{
 					fscanf(arquivo, "%c", &caracterString);
@@ -83,10 +115,12 @@ void mostracomp(FILE* arquivo)
 			}
 			else if (charLido & 0x20)
 			{
+				//le os bytes que indicam o tamanho do inteiro
 				int tamanhoInt = charLido & 0x1f;
 				unsigned char byteInt;
 				int numTotal = 0;
 
+				//com base no tamanho lido, le byte a byte e desloca um byte antes de ler o proximo para numTotal recriar o numero original
 				for (int i = 0; i < tamanhoInt; i++)
 				{
 					numTotal = numTotal << 8;
@@ -94,18 +128,22 @@ void mostracomp(FILE* arquivo)
 					numTotal += byteInt;
 				}
 
+				//trata de numeros negativos com menos de 4 bytes
 				if (tamanhoInt != 4 && (numTotal >> (tamanhoInt - 1)*8) & 0x80)
 				{
 					numTotal += 0xffffffff << (tamanhoInt * 8);
 				}
+				//exibe na tela o numero e seu valor em hexadecimal
 				printf("(int) %-11d (%08x)\n", numTotal, numTotal);
 			}
 			else
 			{
+				//le os bytes que indicam o tamanho do inteiro sem sinal
 				int tamanhoU = charLido & 0x1f;
 				unsigned char byteInt;
 				unsigned numTotal = 0;
 
+				//com base no tamanho lido, le byte a byte e desloca um byte antes de ler o proximo para numTotal recriar o numero original
 				for (int i = 0; i < tamanhoU; i++)
 				{
 					numTotal = numTotal << 8;
@@ -113,6 +151,7 @@ void mostracomp(FILE* arquivo)
 					numTotal += byteInt;
 				}
 
+				//exibe na tela o numero e seu valor em hexadecimal
 				printf("(uns) %-11u (%08x)\n", numTotal, numTotal);
 			}
 		}
@@ -131,14 +170,16 @@ int calculaIndice(int indiceAtual, int tamanhoCampo)
 		return indiceAtual;
 	}
 
+	//muda o indice para ajeitar o padding em caso de um inteiro (precisa iniciar em indice multiplo de 4, seu tamanho em bytes)
 	indiceAtual = indiceAtual + tamanhoCampo - modulo;
 
 	return indiceAtual;
 }
 
-void codificaString(char* str, char* descritor, FILE* arquivo)
+int codificaString(char* str, char* descritor, FILE* arquivo)
 {
 	unsigned char byteCabeca = 0x40, tamanhoStr = 0;
+	int err;
 
 	while (str[tamanhoStr] != '\0')
 	{
@@ -151,19 +192,28 @@ void codificaString(char* str, char* descritor, FILE* arquivo)
 	}
 	byteCabeca += tamanhoStr;
 
-	fprintf(arquivo, "%c", byteCabeca);
+	err = fprintf(arquivo, "%c", byteCabeca);
+	if (err < 0)
+	{
+		return -1;
+	}
 	for (int i = 0; i < tamanhoStr; i++)
 	{
-		fprintf(arquivo, "%c", str[i]);
+		err = fprintf(arquivo, "%c", str[i]);
+		if (err < 0)
+		{
+			return -1;
+		}
 	}
 
-	return;
+	return 0;
 }
 
-void codificaInt(int* num, char* descritor, FILE* arquivo)
+int codificaInt(int* num, char* descritor, FILE* arquivo)
 {
 	unsigned char byteCabeca = 0x20, tamanhoInt = 0;
 	int byteShift;
+	int err;
 
 	if (descritor[1] == '\0')
 	{
@@ -189,19 +239,29 @@ void codificaInt(int* num, char* descritor, FILE* arquivo)
 
 	byteCabeca += tamanhoInt;
 
-	fprintf(arquivo, "%c", byteCabeca);
+	err = fprintf(arquivo, "%c", byteCabeca);
+	if (err < 0)
+	{
+		return -1;
+	}
 	for (int i = 0; i < tamanhoInt; i++)
 	{
 		byteShift = *num >> (tamanhoInt - 1 - i) * 8;
 		byteShift = byteShift & 0xFF;
-		fprintf(arquivo, "%c", byteShift);
+		err = fprintf(arquivo, "%c", byteShift);
+		if (err < 0)
+		{
+			return -1;
+		}
 	}
+	return 0;
 }
 
-void codificaU(unsigned int* num, char* descritor, FILE* arquivo)
+int codificaU(unsigned int* num, char* descritor, FILE* arquivo)
 {
 	unsigned char byteCabeca = 0, tamanhoInt = 0;
 	int byteShift;
+	int err;
 
 	if (descritor[1] == '\0')
 	{
@@ -227,11 +287,20 @@ void codificaU(unsigned int* num, char* descritor, FILE* arquivo)
 
 	byteCabeca += tamanhoInt;
 
-	fprintf(arquivo, "%c", byteCabeca);
+	err = fprintf(arquivo, "%c", byteCabeca);
+	if (err < 0)
+	{
+		return -1;
+	}
 	for (int i = 0; i < tamanhoInt; i++)
 	{
 		byteShift = *num >> (tamanhoInt - 1 - i) * 8;
 		byteShift = byteShift & 0xFF;
-		fprintf(arquivo, "%c", byteShift);
+		err = fprintf(arquivo, "%c", byteShift);
+		if (err < 0)
+		{
+			return -1;
+		}
 	}
+	return 0;
 }
